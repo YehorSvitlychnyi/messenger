24b8e0a0e390f509a88ccf1bc19608b845e22a5a
Vladimir-create-cyber <vlaaadimir1@gmail.com>
2025-03-23 20:02:39 +0200

    create forgot

---
+++ app/views/pages/forgot_page.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Forgot password</title>
+    </head>
+    <body>
+        <h1>Forgot password</h1>
+        <form method="post" class="forget-form">
+            <label for="your login">Your login:</label>
+            <input type="text" name="your login" id="your login" placeholder="your login" autofocus>
+            <label for="secret answer">Secret answer:</label>
+            <input type="text" name="secret answer" id="secret answer" placeholder="secret answer">
+            <button type="submit">Go</button>
+        </form>
+    </body>
+</html>
+
