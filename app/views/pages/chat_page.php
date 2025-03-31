837ec0a7d1ba7d61f8d3d0312af290bbcda43c24
romakholodetskiy <romakholodetskiy@gmail.com>
2025-03-31 16:27:04 +0300

    add

--- app/views/pages/chat_page.php
+++ app/views/pages/chat_page.php
@@ -11,7 +11,7 @@
         <div class="chat-name">
 
         </div>
-        <div class="chat-messages" style="overflow: ">
+        <div class="chat-messages">
 
         </div>
         <div class="chat-new-message">
